7dd515078c6fc6fb68252c0df6c4474b5bd2bfd3
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-25 17:05:24 +0530

    [Ariprasath] added: find the frequency of words in the given paragraph. OOPS concepts used.

---
+++ FrequencyOfWords.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordFrequencyUsingHashTables
+{
+    class FrequencyOfWords
+    {
+        Dictionary<string, int> wordCounts;
+        public FrequencyOfWords()
+        {
+           this.wordCounts = new Dictionary<string, int>();
+        }
+        public void WordCounter(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+            foreach (string elements in words)
+            {
+                if (this.wordCounts.ContainsKey(elements))
+                {
+                    this.wordCounts[elements] += 1;
+                }
+                else
+                {
+                    this.wordCounts[elements] = 1;
+                }
+            }
+            foreach (string key in this.wordCounts.Keys)
+            {
+                Console.WriteLine(key + "-->" + this.wordCounts[key]);
+            }
+
+        }
+    }
+}
---
+++ MainMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordFrequencyUsingHashTables
+{
+    class MainMethod
+    {
+        static void Main(string[] args)
+        {
+            FrequencyOfWords paragraph = new FrequencyOfWords();
+            paragraph.WordCounter("Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations");
+        }
+    }
+}
