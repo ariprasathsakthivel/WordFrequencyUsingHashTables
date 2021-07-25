4dce4f9528ab0b41908d6baf5cd03c314474f033
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-25 16:44:23 +0530

    [Ariprasath] added: find the frequency of words in the given sentance

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WordFrequencyUsingHashTables
 {
@@ -6,7 +7,28 @@ namespace WordFrequencyUsingHashTables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Frequency of words finder program");
+            WordCounter("To be or not to be");
+        }
+        public static void WordCounter(string sentence)
+        {
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+            string[] words = sentence.Split(' ');
+            foreach (string elements in words)
+            {
+                if (wordCounts.ContainsKey(elements))
+                {
+                    wordCounts[elements] += 1;
+                }
+                else
+                {
+                    wordCounts[elements] = 1;
+                }
+            }
+            foreach (string key in wordCounts.Keys)
+            {
+                Console.WriteLine(key+"-->"+wordCounts[key]);
+            }
+
         }
     }
 }
