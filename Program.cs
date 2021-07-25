ead43037a11055d086eeefb423cf55e9bc02bfe6
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-25 16:04:25 +0530

    [Ariprasath] added: Welcome to note to the word frequency finder program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordFrequencyUsingHashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Frequency of words finder program");
+        }
+    }
+}
